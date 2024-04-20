a2cb8f41f6c6864fb698cf714f364ff3b0be7c76
MiaPham2520 <mia2520@uw.edu>
2024-04-20 00:53:23 -0700

    Working app without history

--- LifeCounter/ViewController.swift
+++ LifeCounter/ViewController.swift
@@ -7,18 +7,20 @@
 
 import UIKit
 
+extension String {
+    var isNumber: Bool {
+        let characters = CharacterSet.decimalDigits
+        return CharacterSet(charactersIn: self).isSubset(of: characters)
+    }
+}
+
 class ViewController: UIViewController {
 
-    @IBOutlet weak var lifeCountLabel: UILabel!
-    @IBOutlet weak var lifeCountTwoLabel: UILabel!
     @IBOutlet weak var result: UILabel!
-    @IBOutlet weak var lifeInput: UITextField!
-    @IBOutlet weak var lifeInputTwo: UITextField!
-    
-    
     var lifeCounts: [Int] = []
     var labels: [UILabel] = []
     var lifeInputs: [UITextField] = []
+    var addPlayerBtn = UIButton(type: .roundedRect)
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -37,16 +39,19 @@ class ViewController: UIViewController {
         
 //        self.view.addSubview(mainView)
         
-        var addBtn = UIButton(type: .roundedRect)
-        addBtn.setTitle("Add player", for: .normal)
-        addBtn.tag = 1
+        addPlayerBtn.setTitle("Add player", for: .normal)
+        addPlayerBtn.tag = 1
 //        addBtn.frame = coords[((num-1) / 3)][((num-1) % 3)]
-        addBtn.frame = CGRect(x: 39, y: 59, width:75 , height:39)
-        addBtn.layer.cornerRadius = 10
+        addPlayerBtn.frame = CGRect(x: 39, y: 59, width:75 , height:39)
+        addPlayerBtn.layer.cornerRadius = 10
         
-        self.view.addSubview(addBtn)
+        self.view.addSubview(addPlayerBtn)
         
-        addBtn.addTarget(self, action: #selector(addPlayer(_:)), for: .touchUpInside)
+        addPlayerBtn.addTarget(self, action: #selector(addPlayer(_:)), for: .touchUpInside)
+        
+        for i in 1...4 {
+            addPlayer(addPlayerBtn)
+        }
     }
     
     var playerCount = 0
@@ -58,7 +63,7 @@ class ViewController: UIViewController {
         playerLabel.font = UIFont.boldSystemFont(ofSize: 20)
             
         let lifeCountLabel = UILabel()
-        lifeCountLabel.text = "Life: 20"
+        lifeCountLabel.text = "Life Count: 20"
         lifeCountLabel.font = UIFont.systemFont(ofSize: 16)
         
         let inputTextField = UITextField()
@@ -68,12 +73,12 @@ class ViewController: UIViewController {
         lifeCounts.append(20)
         lifeInputs.append(inputTextField)
         
-        var addBtn = UIButton(type: .roundedRect)
+        let addBtn = UIButton(type: .roundedRect)
         addBtn.setTitle("+", for: .normal)
         addBtn.accessibilityIdentifier = String(playerCount) + "+"
         addBtn.addTarget(self, action: #selector(updateLifeCount(_:)), for: .touchUpInside)
         
-        var minusBtn = UIButton(type: .roundedRect)
+        let minusBtn = UIButton(type: .roundedRect)
         minusBtn.setTitle("-", for: .normal)
         minusBtn.accessibilityIdentifier = String(playerCount) + "-"
         minusBtn.addTarget(self, action: #selector(updateLifeCount(_:)), for: .touchUpInside)
@@ -103,25 +108,31 @@ class ViewController: UIViewController {
         }
         
         playerCount += 1
+        if playerCount > 7 {
+            addPlayerBtn.isEnabled = false
+        }
     }
     
     func displayLife(player: Int) {
         labels[player].text = "Life Count: \(lifeCounts[player])"
-        playerResult()
+        playerResult(player: player)
     }
     
-    func playerResult() {
+    func playerResult(player: Int) {
 //        if lifeCounts[0] <= 0 {
 //            result.text = "Player 1 LOSES"
 //        } else if lifeCounts[1] <= 0 {
 //            result.text = "Player 2 LOSES"
 //        }
+        if lifeCounts[player] <= 0 {
+            result.text = "Player \(player + 1) LOSES"
+        }
     }
     
     //Still need to update this to receive numbers only
     @IBAction func updateLifeCount(_ sender: UIButton) {
-        var playerStr = (sender.accessibilityIdentifier)!.prefix(1)
-        var player: Int = Int(playerStr)!
+        let playerStr = (sender.accessibilityIdentifier)!.prefix(1)
+        let player: Int = Int(playerStr)!
         
         var sign = 1
         
@@ -133,15 +144,26 @@ class ViewController: UIViewController {
         var lifeChange = 1
         
 //        print(lifeChangeText)
-        if (lifeChangeText != "") {
+        if (lifeChangeText != "" && lifeChangeText.isNumber) {
             lifeChange = Int(lifeChangeText)!
+        } else if (lifeChangeText != "") {
+            let alertController = UIAlertController(title: "Error Input", message: "Please Input Number Only", preferredStyle: .alert)
+            let alertAction = UIAlertAction(title: "OK!", style: .default, handler: nil)
+            alertController.addAction(alertAction)
+
+            // Present UIAlertController
+            present(alertController, animated: true, completion: nil)
+            lifeInputs[player].text = ""
+            return
         }
         
         // Read the life change amount from the text field
         lifeCounts[player] += sign  * lifeChange
+        addPlayerBtn.isEnabled = false
         displayLife(player: player)
         // Clear the text field after updating
         lifeInputs[player].text = ""
-        }
+    }
+
 }
 
